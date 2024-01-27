89d00a969cdefc4e074a778260361bffd03972d6
helcio <h.andre@giap.pl>
2024-01-27 11:22:05 +0100

    Complete phase 01

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const { sendFormPage } = require("./routes");
 const { parseBody } = require("./parse-body");
+const http = require("http");
+const { log } = require("console");
 let server;
 
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE ABOVE THIS LINE *******************/
+server = http.createServer((request, response) => {
+  log(request);
+});
 
-// Your code here
+const port = 5000;
 
+server.listen(port, log("Successfully started the server on port 5000 ", port));
 /******************************************************************************/
 /******************* DO NOT CHANGE THE CODE BELOW THIS LINE *******************/
 
